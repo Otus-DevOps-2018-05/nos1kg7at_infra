99f49e7de6bcf81ab5986aa5ad92acb4cc5172b2
Kirill Noskov <noskov.k.g@ulmart.ru>
2018-06-26 01:10:39 +0300

    added files after successful deploy

--- install_mongodb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-sudo echo -e "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt update
-sudo apt install -y mongodb-org
-sudo systemctl enable mongod
-sudo systemctl start mongod
-sudo systemctl status -l mongod
